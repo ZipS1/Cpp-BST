d694b6a10ef70809594c71a0e6484f8af4825f4b
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2023-11-08 12:33:05 +0300

    Implement insertion and general balancing function.
    Working on balancing cases implementation

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -2,11 +2,12 @@
 
 bool BinarySearchTree::insert(int value)
 {
-	SearchResult result = findPlaceFrom(root, value);
+	SearchResult result = findEmptyPlaceFrom(root, value);
 	if (result.node == nullptr)
 		return false;
 
 	result.node = new Node(result.parent, value);
+	ensureBalanced(result.node);
 }
 
 void BinarySearchTree::outputSymmetricWalk()
@@ -31,7 +32,7 @@ BinarySearchTree::~BinarySearchTree()
 {
 }
 
-SearchResult BinarySearchTree::findPlaceFrom(Node* node, int value)
+SearchResult BinarySearchTree::findEmptyPlaceFrom(Node* node, int value)
 {
 	static Node* parent = nullptr;
 
@@ -50,12 +51,91 @@ SearchResult BinarySearchTree::findPlaceFrom(Node* node, int value)
 
 	parent = node;
 	if (value < node->data)
-		findPlaceFrom(node->left, value);
+		findEmptyPlaceFrom(node->left, value);
 	else
-		findPlaceFrom(node->right, value);
+		findEmptyPlaceFrom(node->right, value);
 }
 
-bool BinarySearchTree::ensureBalanced()
+bool BinarySearchTree::ensureBalanced(Node* newNode)
+{
+	bool isBalanceRequired = false;
+	isBalanceRequired = balanceIfCaseFirst(newNode);
+	isBalanceRequired = balanceIfCaseSecond(newNode);
+	isBalanceRequired = balanceIfCaseThird(newNode);
+	isBalanceRequired = balanceIfCaseFourth(newNode);
+	isBalanceRequired = balanceIfCaseFifth(newNode);
+	return isBalanceRequired == false;		
+}
+
+#pragma region BalancingCases
+bool BinarySearchTree::balanceIfCaseFirst(Node* newNode)
+{
+	if (newNode->parent != nullptr)
+		return false;
+
+	newNode->color = Color::Black;
+	return true;
+}
+
+bool BinarySearchTree::balanceIfCaseSecond(Node* newNode)
 {
 	return false;
 }
+
+bool BinarySearchTree::balanceIfCaseThird(Node* newNode)
+{
+	return false;
+}
+
+bool BinarySearchTree::balanceIfCaseFourth(Node* newNode)
+{
+	return false;
+}
+
+bool BinarySearchTree::balanceIfCaseFifth(Node* newNode)
+{
+	return false;
+}
+#pragma endregion
+
+void BinarySearchTree::rotateLeft(Node* node)
+{
+	Node* rightNode = node->right;
+	node->right = rightNode->left;
+	rightNode->left = node;
+	rightNode->parent = node->parent;
+	node->parent = rightNode;
+
+	if (root == node)
+		root = rightNode;
+}
+
+void BinarySearchTree::rotateRight(Node* node)
+{
+	Node* leftNode = node->left;
+	node->left = leftNode->right;
+	leftNode->right = node;
+	leftNode->parent = node->parent;
+	node->parent = leftNode;
+
+	if (root == node)
+		root = leftNode;
+}
+
+Node* BinarySearchTree::getGrandparent(Node* node)
+{
+	if (node != nullptr && node->parent != nullptr)
+		return node->parent->parent;
+	else 
+		return nullptr;
+}
+
+Node* BinarySearchTree::getUncle(Node* node)
+{
+	Node* grandparent = getGrandparent(node);
+	if (grandparent == nullptr)
+		return nullptr;
+
+	return grandparent->right == node->parent ? 
+		grandparent->left : grandparent->right;
+}		
--- BinarySearchTree/BinarySearchTree.h
+++ BinarySearchTree/BinarySearchTree.h
@@ -2,12 +2,14 @@
 
 /* --- УСТРОЙСТВО ДЕРЕВА ---
 * Источник: https://www.youtube.com/watch?v=0BUX_PotA4c
+* Балансировка дерева: https://clck.ru/36Trxy
 * 
 * Красно-черное дерево состоит из красных и черных узлов.
 * Правила КЧ дерева:
 * 1. Листом в КЧ дереве является несуществующий узел (nullptr)
 * 2. Корень и лист черного цвета
 * 3. У красного узла оба ребенка - черные
+* 
 * -------------------------------------------------------------------------
 * Черная высота - количество черных узлов от исходного до null включительно.
 * Исходный узел при этом не считается. Т.е. в дереве формата:
@@ -20,8 +22,14 @@
 * 
 * Черная высота корня равна 1
 * -------------------------------------------------------------------------
-* 4. Черная высота должна быть одинакова для левой/правой стороны,
-*		тогда дерево называется сбалансированным.
+* 
+* 4. Черная высота должна быть одинакова для левой/правой стороны.
+* 
+* -------------------------------------------------------------------------
+* Дерево называется сбалансированным при выполнении правил 1-4.
+* Для балансировки дерева применяются перекрашивания цветов
+* и левые/правые повороты на узлах.
+* -------------------------------------------------------------------------
 */
 
 enum class Color { Red, Black };
@@ -57,8 +65,9 @@ public:
 	/// Inserts value into BST
 	/// </summary>
 	/// <param name="value"></param>
-	/// <returns>True if value inserted. 
-	///				False if value already in tree.
+	/// <returns>
+	/// True if value inserted. 
+	///	False if value already in tree.
 	/// </returns>
 	bool insert(int value);
 	void outputSymmetricWalk();
@@ -78,7 +87,7 @@ private:
 	/// Search result with parent of node place and found node place.
 	///	Both nullptrs if value is already in tree.
 	/// </returns>
-	SearchResult findPlaceFrom(Node* node, int value);
+	SearchResult findEmptyPlaceFrom(Node* node, int value);
 	
 	/// <summary>
 	/// Checks if BST is balanced and balance if it is not
@@ -87,5 +96,68 @@ private:
 	/// True if BST was already balanced. 
 	/// False otherwise. 
 	/// </returns>
-	bool ensureBalanced();
+	bool ensureBalanced(Node* newNode);
+
+	/// <summary>
+	/// Balance tree if new node is root of tree
+	/// </summary>
+	/// <param name="newNode">that was inserted</param>
+	/// <returns>True if balance required, false otherwise</returns>
+	bool balanceIfCaseFirst(Node* newNode);
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="newNode">that was inserted</param>
+	/// <returns>True if balance required, false otherwise</returns>
+	bool balanceIfCaseSecond(Node* newNode);
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="newNode">that was inserted</param>
+	/// <returns>True if balance required, false otherwise</returns>
+	bool balanceIfCaseThird(Node* newNode);
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="newNode">that was inserted</param>
+	/// <returns>True if balance required, false otherwise</returns>
+	bool balanceIfCaseFourth(Node* newNode);
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="newNode">that was inserted</param>
+	/// <returns>True if balance required, false otherwise</returns>
+	bool balanceIfCaseFifth(Node* newNode);
+
+	
+
+	/// <summary>
+	/// Perform left rotation on node.
+	/// </summary>
+	/// <param name="node">to perform rotation on</param>
+	void rotateLeft(Node* node);
+
+	/// <summary>
+	/// Perform right rotation on node.
+	/// </summary>
+	/// <param name="node">to perform rotation on</param>
+	void rotateRight(Node* node);
+	
+	/// <summary>
+	/// Returns grandparent of node. Grandparent is parent of parent of node
+	/// </summary>
+	/// <param name="node">of which to get grandparent</param>
+	/// <returns>Grandparent of node OR  nullptr if grandparent does not exist</returns>
+	Node* getGrandparent(Node* node);
+
+	/// <summary>
+	/// Returns uncle of node. Uncle is child of grandparent which is not parent of node
+	/// </summary>
+	/// <param name="node">of which to get uncle</param>
+	/// <returns>Uncle of node OR nullptr if uncle does not exist</returns>
+	Node* getUncle(Node* node);
 };
